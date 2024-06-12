52fc52cb85d18ba4632382a0405804882d1fe5e3
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-06-12 15:09:31 +0200

    generic_tasks_done

---
+++ src/generics/6.ts
@@ -0,0 +1,17 @@
+type Errors = {
+  email?: string[];
+  firstName?: string[];
+  lastName?: string[];
+  phone?: string[];
+};
+
+type Form = {
+  email: string | null;
+  firstName: string | null;
+  lastName: string | null;
+  phone: string | null;
+  errors: Errors;
+};
+
+// тип Params, який включає всі поля з Form, крім errors
+type Params = Omit<Form, "errors">;
